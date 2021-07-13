899fedd31fb9bf8568b178325e90081b9e46b17f
Thiago Macedo Simões <80564497+thiagomacedosimoes@users.noreply.github.com>
2021-07-12 23:12:28 -0300

    Condicionais, laços de repetição e Date

---
+++ js/main.js
@@ -0,0 +1,60 @@
+
+
+
+/*
+
+    //IF-ELSE
+
+var idade = prompt("Quantos anos você tem?");
+
+if (idade >= 18) {
+    alert("Maior de idade");
+}
+else{
+    alert("Menor de idade");
+};
+*/
+
+/*
+
+    //Laço WHILE
+
+var count = 0;
+
+while (count <= 5 ){
+    console.log(count);
+    alert(count);
+    count++;
+}
+*/
+
+
+/*
+
+    //Laço FOR
+
+var count;
+
+for (count = 0; count <= 5; count++)
+{
+    alert(count); 
+};
+*/
+
+
+/*
+
+    //Date
+
+
+var d = new Date();
+
+alert(d);
+alert(d.getFullYear()); //Ano
+alert(d.getMonth()+1); //Mês
+alert(d.getDate()); //Dia
+alert(d.getMinutes()); //Minutos
+alert(d.getSeconds()); //Segundos
+*/
+
+
